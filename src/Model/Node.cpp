494591d5de13ef55c3d316171a61dc97d6355f19
Johan L <61603601+JTRON780@users.noreply.github.com>
2026-05-07 17:08:43 -0400

    Updated Node

--- src/Model/Node.cpp
+++ src/Model/Node.cpp
@@ -1,1 +1,30 @@
-//Node.cpp
+#include "Model/Node.h"
+
+namespace av::model {
+
+std::string Node::displayLabel() const {
+    return label.empty() ? id : label;
+}
+
+std::string toString(const NodeState state) {
+    switch (state) {
+        case NodeState::Normal:
+            return "normal";
+        case NodeState::Start:
+            return "start";
+        case NodeState::Frontier:
+            return "frontier";
+        case NodeState::Active:
+            return "active";
+        case NodeState::Visited:
+            return "visited";
+        case NodeState::Path:
+            return "path";
+        case NodeState::MinimumSpanningTree:
+            return "mst";
+    }
+
+    return "unknown";
+}
+
+}  // namespace av::model
